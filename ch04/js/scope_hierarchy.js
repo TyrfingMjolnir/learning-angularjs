7de221bdfaeca160101a2944c286bd91cab86dcc
Tyrfing Mjolner <4lphamal3@gmail.com>
2015-03-16 11:11:11 +0800

    doomified() and aligned

--- ch04/js/scope_hierarchy.js
+++ ch04/js/scope_hierarchy.js
@@ -1,22 +1,22 @@
-angular.module('myApp', []).
-  controller('LevelA', function($scope) {
-    $scope.title = "Level A"
+angular.module( 'myApp', [] ).
+  controller( 'LevelA', function( $scope ) {
+    $scope.title  = "Level A"
     $scope.valueA = 1;
-    $scope.inc = function() {
+    $scope.inc    = function() {
       $scope.valueA++;
     };
   }).
-  controller('LevelB', function($scope) {
-    $scope.title = "Level B"
+  controller( 'LevelB', function( $scope ) {
+    $scope.title  = "Level B"
     $scope.valueB = 1;
-    $scope.inc = function() {
+    $scope.inc    = function() {
       $scope.valueB++;
     };
   }).
-  controller('LevelC', function($scope) {
-    $scope.title = "Level C"
+  controller( 'LevelC', function( $scope ) {
+    $scope.title  = "Level C"
     $scope.valueC = 1;
-    $scope.inc = function() {
+    $scope.inc    = function() {
       $scope.valueC++;
     };
   });
