320aaaae65d64346cf4495e4e0e91045c780c209
Tyrfing Mjolner <4lphamal3@gmail.com>
2015-03-16 13:12:55 +0800

    doomified() and aligned

--- ch06/js/directive_form.js
+++ ch06/js/directive_form.js
@@ -1,8 +1,8 @@
 angular.module( 'myApp', [] ).
   controller(   'myController', function( $scope ) {
     $scope.cameras = [
-      { make:'Canon', model:'70D', mp:20.2 },
-      { make:'Canon', model:'6D', mp:20 },
+      { make:'Canon', model:'70D',   mp:20.2 },
+      { make:'Canon', model:'6D',    mp:20 },
       { make:'Nikon', model:'D7100', mp:24.1 },
       { make:'Nikon', model:'D5200', mp:24.1 }
     ];
