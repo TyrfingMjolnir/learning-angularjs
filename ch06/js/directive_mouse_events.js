e2dd9715d89614010378f92657266d6cc714e8ad
Tyrfing Mjolner <4lphamal3@gmail.com>
2015-03-16 13:09:37 +0800

    doomified() and aligned

--- ch06/js/directive_mouse_events.js
+++ ch06/js/directive_mouse_events.js
@@ -1,14 +1,14 @@
-angular.module('myApp', []).
-  controller('myController', function($scope) {
-    $scope.mouseInfo = {};
+angular.module( 'myApp', []).
+  controller(   'myController', function( $scope ) {
+    $scope.mouseInfo     = {};
     $scope.lastClickInfo = {};
-    $scope.mouseClick = function(event){
+    $scope.mouseClick    = function( event ) {
       $scope.lastClickInfo.clientX = event.clientX;
       $scope.lastClickInfo.clientY = event.clientY;
       $scope.lastClickInfo.screenX = event.screenX;
       $scope.lastClickInfo.screenY = event.screenY;
     };
-    $scope.mouseMove = function(event){
+    $scope.mouseMove = function( event ) {
       $scope.mouseInfo.clientX = event.clientX;
       $scope.mouseInfo.clientY = event.clientY;
       $scope.mouseInfo.screenX = event.screenX;
